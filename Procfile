2d7503d4e3e4240704be3ccfec347f4a270cfe9f
Abhishek42001 <jaiswalabhishek508@gmail.com>
2022-03-10 01:54:21 +0530

    changes

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn findhome.wsgi --log-file -
--- findhome/settings.py
+++ findhome/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,15 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2yze$us832%6)si15hib9i(_n1sjq8^g)t2)b*+90uik1ekp=-'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['192.168.105.69','192.168.43.215']
 
 
+ALLOWED_HOSTS = ['findhome.herokuapp.com']
+SECRET_KEY=config("SECRET_KEY")
+DEBUG=config("DEBUG",default=True,cast=bool)
 # Application definition
 
 INSTALLED_APPS = [
@@ -87,12 +85,14 @@ WSGI_APPLICATION = 'findhome.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 DATABASES = {
