0564846d83755ba8097b7e9600cdcd3e9882f90c
Abhishek42001 <jaiswalabhishek508@gmail.com>
2022-03-10 02:51:34 +0530

    changes

--- findhome/urls.py
+++ findhome/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('sendmessage',include('chat.urls')),
     path('getallchatsbyid',include('getallchatsbyid.urls')),
     path('getallchatsbetweentwoid',include('allchatsbtwntwoid.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
