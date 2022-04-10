0ef138530c79caccc4a5ef31894453db67da1463
Abhishek42001 <jaiswalabhishek508@gmail.com>
2022-04-10 22:31:14 +0530

    changes

--- updateapplied/views.py
+++ updateapplied/views.py
@@ -19,10 +19,10 @@ def updateCover(request):
         #     main_image=data.get("image"),created_date=datetime.datetime.now()
         # )
         print(data.get('user_id'))
-        print(data.get('id'))        
+        print(data.get('database_id'))        
         print(data.get('image'))
         print(data.get("public_id"))
-        Apply.objects.filter(user_id=data.get('user_id'),id=data.get('id')).update(
+        Apply.objects.filter(user_id=data.get('user_id'),id=data.get('database_id')).update(
             main_image=cloudinary.uploader.upload(data.get('image'),public_id=data.get("public_id")),
             created_date=timezone.now()
         )
